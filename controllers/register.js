3cef58e31c6cee844f4f1946a4877ced98b23cba
manuelLanderos <108149974+manuelLanderos@users.noreply.github.com>
2023-05-30 14:50:07 -0400

    yes

--- controllers/register.js
+++ controllers/register.js
@@ -1,20 +1,14 @@
 const handleRegister = (req, res, db, bcrypt) => {
-  const { email, password, name } = req.body;
-
-  console.log('Email:', email);
-  console.log('Name:', name);
-  console.log('Password:', password);
-
+  const { email, password, name } = req.body
   if (!email || !name || !password) {
-    console.log('Incorrect form submission');
-    return res.status(400).json('incorrect form submission');
+    return res.status(400).json("incorrect form submission")
   }
+  const hash = bcrypt.hashSync(password)
 
-  const hash = bcrypt.hashSync(password);
-
+  // bcrypt.hash(password, null, null, function (err, hash) {
+  //   console.log(hash)
+  // });
   db.transaction(trx => {
-    console.log('Transaction started');
-
     trx.insert({
       hash: hash,
       email: email
@@ -22,33 +16,24 @@ const handleRegister = (req, res, db, bcrypt) => {
       .into('login')
       .returning('email')
       .then(loginEmail => {
-        console.log('Inserted into login table:', loginEmail);
-
         return trx('users')
           .returning('*')
           .insert({
             email: loginEmail[0].email,
             name: name,
             joined: new Date()
+
           })
           .then(user => {
-            console.log('Inserted into users table:', user);
-            res.json(user[0]);
+            res.json(user[0])
           })
       })
       .then(trx.commit)
-      .catch(err => {
-        console.log('Transaction error:', err);
-        trx.rollback();
-        res.status(400).json('unable to register');
-      });
+      .catch(trx.rollback)
   })
-    .catch(err => {
-      console.log('Database connection error:', err);
-      res.status(400).json('unable to register');
-    });
-};
+    .catch(err => res.status(400).json('unable to work'))
 
+}
 module.exports = {
   handleRegister: handleRegister
-};
+}
