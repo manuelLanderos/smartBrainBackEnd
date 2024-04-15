de5d1098a945191a83a168cfb8b8ea5e669dab95
ManuelLanderos <manuel.landeros@icloud.com>
2024-04-15 19:46:05 -0400

    newest changes

--- server.js
+++ server.js
@@ -1,63 +1,41 @@
-// this is the new yes
-// new new
-const express = require('express')
-const bodyParser = require('body-parser')
-const app = express()
-const bcrypt = require('bcrypt-nodejs')
-const cors = require('cors')
-const knex = require('knex')
-const register = require('./controllers/register')
-const signIn = require('./controllers/signIn')
-const profile = require('./controllers/profile')
-const image = require('./controllers/image')
-// const app = require('../server');
-// const db = knex({
-//   client: 'pg',
-//   connection: {
-//     host: '127.0.0.1', //localhost
-//     user: 'manuel', //add your user name for the database here
-//     port: 54322, // add your port number here
-//     password: '', //add your correct password in here
-//     database: 'smart-brain' //add your database name you created here
-//   }
-// });
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const bcrypt = require('bcrypt-nodejs');
+const cors = require('cors');
+const knex = require('knex');
+const register = require('./controllers/register');
+const signIn = require('./controllers/signIn');
+const profile = require('./controllers/profile');
+const image = require('./controllers/image');
 
-let db;
+// Initialize database connection
+const db = knex({
+  client: 'pg',
+  connection: {
+    host: 'dpg-cntlfcmn7f5s7382aq40-a',
+    user: 'smart_brain_database_oghq_user',
+    password: '2ygVbC46YIqmpoQNXbR5ServWQxCeAZQ',
+    database: 'smart_brain_database_oghq'
+  }
+});
 
-try {
-  db = knex({
-    client: 'pg',
-    connection: {
-      host: 'dpg-cntlfcmn7f5s7382aq40-a',
-      user: 'smart_brain_database_oghq_user',
-      password: '2ygVbC46YIqmpoQNXbR5ServWQxCeAZQ',
-      database: 'smart_brain_database_oghq'
-    }
-  });
-} catch (error) {
-  console.error('something is wrong:', error);
-  // Handle the error gracefully, e.g., by shutting down the server or logging the error.
-}
+// Middleware
+app.use(bodyParser.json());
+app.use(cors());
 
-// db.select('*').from('users').then(data => {
-//   console.log(data)
-// })
+// Routes
+app.get('/', (req, res) => { res.send(db.users) });
+app.post('/signin', (req, res) => { signIn.handleSignIn(req, res, db, bcrypt); });
+app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt); });
+app.get('/profile/:id', (req, res) => { profile.handleProfile(req, res, db); });
+app.put('/image', (req, res) => { image.handleImage(req, res, db); });
+app.post('/imageurl', (req, res) => { image.handleApiCall(req, res); });
 
-app.use(bodyParser.json())
-app.use(cors())
-
-
-
-app.get('/', (req, res) => { res.send(db.users) })
-app.post('/signin', (req, res) => { signIn.handleSignIn(req, res, db, bcrypt) })
-app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
-app.get('/profile/:id', (req, res) => { profile.handleProfile(req, res, db) })
-app.put('/image', (req, res) => { image.handleImage(req, res, db) })
-app.post('/imageurl', (req, res) => { image.handleApiCall(req, res) });
-app.listen(3000, () => {
-  console.log('app is running on port 3000')
-
-})
+// Start server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`app is running on port ${PORT}`);
+});
 
 module.exports = app;
-
